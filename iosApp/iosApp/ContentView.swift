3d0d12bf733791443f451f22afd218c067a16e29
Angelo Melo <angelorobsonmelo@gmail.com>
2022-02-15 10:32:30 +0000

    consuming services on both plataforms

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,11 +2,27 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+	let greeting = Greeting()
+    
+    @State var greet = "Loading..."
+    
+    func load() {
+        greeting.greeting {result, error in
+            if let result = result {
+                
+                self.greet = result
+            } else if let error = error {
+                greet = "Error \(error)"
+            }
+        }
+    }
 
 	var body: some View {
-		Text(greet)
-	}
+        
+        Text(greet).onAppear {
+            load()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
