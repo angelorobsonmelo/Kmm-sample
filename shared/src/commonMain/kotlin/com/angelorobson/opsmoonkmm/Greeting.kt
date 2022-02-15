5e12cd9a3025f88d01b3d42a629a8f9570d24c79
Angelo Melo <angelorobsonmelo@gmail.com>
2022-02-15 11:32:47 +0000

    Implemented serializable for both platforms

--- shared/src/commonMain/kotlin/com/angelorobson/opsmoonkmm/Greeting.kt
+++ shared/src/commonMain/kotlin/com/angelorobson/opsmoonkmm/Greeting.kt
@@ -2,33 +2,45 @@ package com.angelorobson.opsmoonkmm
 
 import io.github.aakira.napier.Napier
 import io.ktor.client.*
+import kotlinx.serialization.Serializable
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
+import kotlinx.serialization.json.Json
 
+
+@Serializable
+data class Todo(
+    val title: String
+)
 
 class Greeting {
 
     private val httpclient = HttpClient() {
         install(Logging) {
-            level = LogLevel.HEADERS
+            level = LogLevel.BODY
             logger = object : Logger {
                 override fun log(message: String) {
                     Napier.v(tag = "HTTP Client", message = message)
                 }
-
             }
+        }
+        install(JsonFeature) {
+            val json = Json { ignoreUnknownKeys = true }
+            serializer = KotlinxSerializer(json = json)
         }
     }.also {
         initLogger()
     }
 
+    @Throws(Exception::class)
     suspend fun greeting(): String {
-        return "${getJson()}, ${Platform().platform}!"
+        return "${getTodoList().random().title}, ${Platform().platform}!"
     }
 
-    private suspend fun getJson(): String {
-        val response: HttpResponse = httpclient.get("https://jsonplaceholder.typicode.com/todos")
-        return response.readText()
+    private suspend fun getTodoList(): List<Todo> {
+        return httpclient.get("https://jsonplaceholder.typicode.com/todos")
+
     }
 }
